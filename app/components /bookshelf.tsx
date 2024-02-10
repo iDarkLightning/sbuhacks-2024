113d1275bdd60a5b48157638427392fa3ce15e55
awareermine <aramie.ewen@gmail.com>
2024-02-09 22:57:00 -0500

    add gap back

--- app/components /bookshelf.tsx
+++ app/components /bookshelf.tsx
@@ -26,7 +26,7 @@ export const Bookshelf = () => {
   }, []);
 
   return (
-    <div className="flex flex-col p-1 bg-orange-900 rounded-sm shadow-md">
+    <div className="flex flex-col gap-1 p-1 bg-orange-900 rounded-sm shadow-md">
       {shelves.map((shelf) => (
         <div className="flex flex-row scrollbar-thumb-orange-900 scrollbar-track-orange-800 scrollbar-thin scrollbar-rounded-lg overflow-x-scroll overflow-y-clip max-h-[15ch] bg-neutral-100 rounded-sm gap-1">
           {shelf.map((book) => (
