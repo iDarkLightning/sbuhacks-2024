d4426415eac9698c9d5bfc32ef1602a2fcd86a88
awareermine <aramie.ewen@gmail.com>
2024-02-09 22:54:24 -0500

    scrollbar better

--- app/components /bookshelf.tsx
+++ app/components /bookshelf.tsx
@@ -4,13 +4,31 @@ export const Bookshelf = () => {
   const [shelves, setShelves] = useState<string[][]>([]);
 
   useEffect(() => {
-    setShelves([["Piranesi", "The Memory Theater"], ["ACOTAR"]]);
+    setShelves([
+      [
+        "Piranesi",
+        "The Memory Theater",
+        "Piranesi",
+        "The Memory Theater",
+        "Piranesi",
+        "The Memory Theater",
+        "Lorem Ipsum",
+        "Piranesi",
+        "The Memory Theater",
+        "Piranesi",
+        "The Memory Theater",
+        "Piranesi",
+        "The Memory Theater",
+        "Lorem Ipsum",
+      ],
+      ["ACOTAR"],
+    ]);
   }, []);
 
   return (
-    <div className="flex flex-col gap-1 p-1 bg-orange-800 rounded-sm shadow-md">
+    <div className="flex flex-col p-1 bg-orange-900 rounded-sm shadow-md">
       {shelves.map((shelf) => (
-        <div className="flex flex-row max-h-[15ch] bg-neutral-100 rounded-sm gap-1">
+        <div className="flex flex-row scrollbar-thumb-orange-900 scrollbar-track-orange-800 scrollbar-thin scrollbar-rounded-lg overflow-x-scroll overflow-y-clip max-h-[15ch] bg-neutral-100 rounded-sm gap-1">
           {shelf.map((book) => (
             <div className="w-fit rounded-sm [user-select:none] p-1 whitespace-nowrap [writing-mode:vertical-rl] bg-neutral-300">
               {book}
