a8b5a70ac8764f25cde0bf1b1108937fbf491a90
Nirjhor Nath <nirjhor.nath@gmail.com>
2024-02-10 11:29:02 -0500

    remove ensureData

--- app/routes/main.tsx
+++ app/routes/main.tsx
@@ -5,6 +5,5 @@ import { HomeView } from "../views/home";
 export const homeRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/",
-  loader: async ({ context }) => await context.queryUtils.test.ensureData(),
   component: HomeView,
 });
